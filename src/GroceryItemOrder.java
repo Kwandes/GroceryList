947fff98bd69b8419a0e7445decacfc6b32f0a08
AlexM <Nyt604@gmail.com>
2020-02-06 16:37:26 +0100

    Finished GroceryItemOrder Class with Getters and Setters

--- src/GroceryItemOrder.java
+++ src/GroceryItemOrder.java
@@ -9,4 +9,23 @@ public class GroceryItemOrder {
         this.price = price;
     }
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price = price;
+    }
 }
