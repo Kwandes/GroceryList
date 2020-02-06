716657da0ba127d8df7efe9cc1f483a1afcac433
AlexM <Nyt604@gmail.com>
2020-02-06 16:34:29 +0100

    what even

---
+++ src/GroceryItemOrder.java
@@ -0,0 +1,12 @@
+public class GroceryItemOrder {
+
+    private String name;
+    private int price;
+
+    public GroceryItemOrder(String name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+}
